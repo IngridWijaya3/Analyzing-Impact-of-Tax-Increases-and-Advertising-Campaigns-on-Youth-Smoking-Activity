e47396dea1467dd476ff1878ffdc71736d30d721
Ingrid Wijaya <ingridwijaya3@gmail.com>
2016-12-15 17:32:13 -0500

    put folder name and fix tax rate file name

--- Python Codes/YouthSmokingAnalysis.py
+++ Python Codes/YouthSmokingAnalysis.py
@@ -4,15 +4,15 @@ import matplotlib
 import datetime
 
 class YouthSmokingAnalysis:
-    csvFileName="YTS_Clean.csv"
-    taxRateCsvFileName="tax_data_all.csv"
+    ytsCsvFileName="YTS_Clean.csv"
+    taxRateCsvFileName="tax_data_all_cleaned.csv"
     outputCSVFolderName= "AnalysisCSV"
     outputPlotFolderName="Plots"
     def __init__(self):
-        self.ytsDataFrame= pandas.read_csv("YTS_Clean.csv")
-        self.taxRateDataFrame= pandas.read_csv("tax_data_all.csv")
+        self.ytsDataFrame= pandas.read_csv(YouthSmokingAnalysis.ytsCsvFileName)
+        self.taxRateDataFrame= pandas.read_csv(YouthSmokingAnalysis.taxRateCsvFileName)
         self.YTSAndTaxRateDataFrame=pandas.merge( self.ytsDataFrame, self.taxRateDataFrame, left_on=['YEAR','LocationDesc'], right_on=['YEAR','STATE'])
-        print(self.YTSAndTaxRateDataFrame)
+       
     def analyze(self):
         pass
     def analyzeCessation(self):
