23fe9f477ec963c8b62393dca2faf11d97cf4b80
Ingrid Wijaya <ingridwijaya3@gmail.com>
2016-12-15 12:03:33 -0500

    state analysis before and after the campaign

--- Python Codes/StateAnalysis.py
+++ Python Codes/StateAnalysis.py
@@ -12,12 +12,11 @@ class StateAnalysis(YouthSmokingAnalysis):
     def analyze(self):
         yearandStateGroup=self.ytsDataFrame[(self.ytsDataFrame.Gender=='Overall')
                                              & (self.ytsDataFrame.TopicDesc=='Cigarette Use (Youth)' )
-                                             & (self.ytsDataFrame.MeasureDesc=='Smoking Status')
-                                             & (self.ytsDataFrame.Response=='Ever')]
-        yearandStateGroup["NumberOfPeople"]=(yearandStateGroup.Data_Value*yearandStateGroup.Sample_Size)/100
-        print(yearandStateGroup[['YEAR','LocationAbbr','Data_Value','Sample_Size','NumberOfPeople']])
-        numberOfPeopleSmoking=yearandStateGroup.groupby(['YEAR','LocationAbbr'], as_index=False)['NumberOfPeople'].sum()
-        pivotTable=pandas.pivot_table(numberOfPeopleSmoking, index=['YEAR'], columns=['LocationAbbr'] , values='NumberOfPeople')
+                                             & (self.ytsDataFrame.MeasureDesc=='Smoking Status')]
+       
+        print(yearandStateGroup[['YEAR','LocationAbbr','Data_Value']])
+        numberOfPeopleSmoking=yearandStateGroup.groupby(['YEAR','LocationAbbr'], as_index=False)['Data_Value'].mean()
+        pivotTable=pandas.pivot_table(numberOfPeopleSmoking, index=['YEAR'], columns=['LocationAbbr'] , values='Data_Value')
 
 
         print(numberOfPeopleSmoking)
@@ -28,12 +27,62 @@ class StateAnalysis(YouthSmokingAnalysis):
     def analyzeNonCessation(self):
         print("add codes here")
     def analyzeBeforeAndAfterCampaign(self):
-        print("add codes here")
+       
+        self.stateAndCigaretteUseAfterCampaign=self.ytsDataFrame[(self.ytsDataFrame.Gender=='Overall')
+                                             & (self.ytsDataFrame.TopicDesc=='Cigarette Use (Youth)' )
+                                             & (self.ytsDataFrame.MeasureDesc=='Smoking Status')
+                                             & (self.ytsDataFrame.YEAR>2012)][['YEAR','LocationAbbr','Data_Value']]
+        self.stateAndCigaretteUseAfterCampaign=self.stateAndCigaretteUseAfterCampaign.rename(columns={"Data_Value": "AverageAfterCampaign"}).groupby(['LocationAbbr'])['AverageAfterCampaign'].mean()
+        
+        self.stateAndCigaretteUseBeforeCampaign=self.ytsDataFrame[(self.ytsDataFrame.Gender=='Overall')
+                                             & (self.ytsDataFrame.TopicDesc=='Cigarette Use (Youth)' )
+                                             & (self.ytsDataFrame.MeasureDesc=='Smoking Status')
+                                             & (self.ytsDataFrame.YEAR<=2012)][['YEAR','LocationAbbr','Data_Value']]
+        self.stateAndCigaretteUseBeforeCampaign=self.stateAndCigaretteUseBeforeCampaign.rename(columns={"Data_Value": "AverageBeforeCampaign"}).groupby(['LocationAbbr'])['AverageBeforeCampaign'].mean()
+      
+ 
+        self.stateCigaretteUseResult= pandas.concat([self.stateAndCigaretteUseBeforeCampaign,self.stateAndCigaretteUseAfterCampaign ],axis=1)
+        self.stateCigaretteUseResult["PercentDecrease"]=((self.stateCigaretteUseResult.AverageBeforeCampaign-self.stateCigaretteUseResult.AverageAfterCampaign)/self.stateCigaretteUseResult.AverageBeforeCampaign)*100
+        self.stateCigaretteUseResult=self.stateCigaretteUseResult.sort_values(by='PercentDecrease', ascending=False)
+
+        self.stateAndSmokelessTobaccoUseBeforeCampaign=self.ytsDataFrame[(self.ytsDataFrame.Gender=='Overall')
+                                             & (self.ytsDataFrame.TopicDesc=='Smokeless Tobacco Use (Youth)' )
+                                             & (self.ytsDataFrame.YEAR<=2012)][['YEAR','LocationAbbr','Data_Value']]
+        self.stateAndSmokelessTobaccoUseBeforeCampaign=self.stateAndSmokelessTobaccoUseBeforeCampaign.rename(columns={"Data_Value": "AverageBeforeCampaign"}).groupby(['LocationAbbr'])['AverageBeforeCampaign'].mean()
+
+        self.stateAndSmokelessTobaccoUseAfterCampaign=self.ytsDataFrame[(self.ytsDataFrame.Gender=='Overall')
+                                             & (self.ytsDataFrame.TopicDesc=='Smokeless Tobacco Use (Youth)' )
+                                             & (self.ytsDataFrame.YEAR>2012)][['YEAR','LocationAbbr','Data_Value']].rename(columns={"Data_Value": "AverageAfterCampaign"}).groupby(['LocationAbbr'])['AverageAfterCampaign'].mean()
+
+        self.stateSmokelessTobaccoUseResult= pandas.concat([self.stateAndSmokelessTobaccoUseBeforeCampaign,self.stateAndSmokelessTobaccoUseAfterCampaign],axis=1)
+        self.stateSmokelessTobaccoUseResult["PercentDecrease"]=((self.stateSmokelessTobaccoUseResult.AverageBeforeCampaign-self.stateSmokelessTobaccoUseResult.AverageAfterCampaign)/self.stateSmokelessTobaccoUseResult.AverageBeforeCampaign)*100
+        self.stateSmokelessTobaccoUseResult=self.stateSmokelessTobaccoUseResult.sort_values(by='PercentDecrease', ascending=False)
+        
+        self.stateAndCessationBeforeCampaign=self.ytsDataFrame[(self.ytsDataFrame.Gender=='Overall')
+                                             & (self.ytsDataFrame.TopicDesc=='Cessation (Youth)' )
+                                             & (self.ytsDataFrame.YEAR<=2012)][['YEAR','LocationAbbr','Data_Value']]
+        self.stateAndCessationBeforeCampaign=self.stateAndCessationBeforeCampaign.rename(columns={"Data_Value": "AverageBeforeCampaign"}).groupby(['LocationAbbr'])['AverageBeforeCampaign'].mean()
+
+        self.stateAndCessationAfterCampaign=self.ytsDataFrame[(self.ytsDataFrame.Gender=='Overall')
+                                             & (self.ytsDataFrame.TopicDesc=='Cessation (Youth)' )
+                                             & (self.ytsDataFrame.YEAR>2012)][['YEAR','LocationAbbr','Data_Value']]
+        self.stateAndCessationAfterCampaign=self.stateAndCessationAfterCampaign.rename(columns={"Data_Value": "AverageAfterCampaign"}).groupby(['LocationAbbr'])['AverageAfterCampaign'].mean()
+
+        self.stateCessationTobaccoUseResult= pandas.concat([self.stateAndCessationBeforeCampaign,self.stateAndCessationAfterCampaign],axis=1)
+        self.stateCessationTobaccoUseResult["PercentIncrease"]=((self.stateCessationTobaccoUseResult.AverageAfterCampaign-self.stateCessationTobaccoUseResult.AverageBeforeCampaign)/self.stateCessationTobaccoUseResult.AverageBeforeCampaign)*100
+        self.stateCessationTobaccoUseResult=self.stateCessationTobaccoUseResult.sort_values(by='PercentIncrease', ascending=False)
+        
+        print(self.stateCigaretteUseResult)
+        print(self.stateSmokelessTobaccoUseResult)
+        print(self.stateCessationTobaccoUseResult)
+
+
+        
     def analyzeBeforeAndAfterTax(self):
         print("add codes here")
     def plotResult(self):
         print("add codes here")
 
 a=StateAnalysis()
-a.analyze()
+a.analyzeBeforeAndAfterCampaign()
 
