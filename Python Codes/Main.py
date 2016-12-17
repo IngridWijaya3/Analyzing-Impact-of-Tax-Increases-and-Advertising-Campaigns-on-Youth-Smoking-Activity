9bce704aaa7ac521c213008332cc186e40e8f7f9
cho11021009 <cho11021009@gmail.com>
2016-12-17 17:52:24 -0500

    Add files via upload

--- Python Codes/Main.py
+++ Python Codes/Main.py
@@ -1,6 +1,11 @@
 
 from StateCampaignAnalysis import StateCampaignAnalysis
+from GenderAnalysis import GenderAnalysis
+
 
 a=StateCampaignAnalysis()
 a.analyze()
+
+b = GenderAnalysis()
+b.analyze()
 
